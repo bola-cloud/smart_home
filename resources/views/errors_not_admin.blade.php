aa81938d14aab8eac42b48b604aacae561da26c7
bola-cloud <bola.ishak41@gmail.com>
2024-10-20 12:36:46 +0300

    commit

--- resources/views/errors_not_admin.blade.php
+++ resources/views/errors_not_admin.blade.php
@@ -46,37 +46,6 @@ data-open="click" data-menu="vertical-content-menu" data-col="1-column">
                 <h2 class="error-code text-center mb-2">400</h2>
                 <h3 class="text-uppercase text-center">You aren't admin</h3>
               </div>
-              <div class="card-content">
-                
-                <div class="row py-2">
-                  <div class="col-12 col-sm-6 col-md-6">
-                    <a href="index.html" class="btn btn-primary btn-block"><i class="ft-home"></i> Back to Home</a>
-                  </div>
-                  <div class="col-12 col-sm-6 col-md-6">
-                    <a href="search-website.html" class="btn btn-danger btn-block"><i class="ft-search"></i>  Advanced search</a>
-                  </div>
-                </div>
-              </div>
-              <div class="card-footer bg-transparent">
-                <div class="row">
-                  <p class="text-muted text-center col-12 py-1">© 2018 <a href="#">Modern </a>Crafted with <i class="ft-heart pink"> </i>                    by <a href="http://themeforest.net/user/pixinvent/portfolio"
-                    target="_blank">PIXINVENT</a></p>
-                  <div class="col-12 text-center">
-                    <a href="#" class="btn btn-social-icon mr-1 mb-1 btn-outline-facebook">
-                      <span class="la la-facebook"></span>
-                    </a>
-                    <a href="#" class="btn btn-social-icon mr-1 mb-1 btn-outline-twitter">
-                      <span class="la la-twitter"></span>
-                    </a>
-                    <a href="#" class="btn btn-social-icon mr-1 mb-1 btn-outline-linkedin">
-                      <span class="la la-linkedin font-medium-4"></span>
-                    </a>
-                    <a href="#" class="btn btn-social-icon mr-1 mb-1 btn-outline-github">
-                      <span class="la la-github font-medium-4"></span>
-                    </a>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
         </section>
