034c624b7c01c67ad7f5acf4689ea87865d40dcb
bola-cloud <bola.ishak41@gmail.com>
2024-10-27 13:51:55 +0300

    member

--- app/Http/Controllers/Api/Devices/DeviceController.php
+++ app/Http/Controllers/Api/Devices/DeviceController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Device;
+use App\Models\Member;
 use App\Models\Section;
 
 class DeviceController extends Controller
