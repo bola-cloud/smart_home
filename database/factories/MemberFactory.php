95371919dfd3285aefe0d6f1d205b08a14d0079e
bola-cloud <bola.ishak41@gmail.com>
2024-10-27 12:00:42 +0300

    remove member

--- database/factories/MemberFactory.php
+++ database/factories/MemberFactory.php
@@ -3,8 +3,9 @@
 namespace Database\Factories;
 
 use App\Models\Member;
+use App\Models\User;
+use App\Models\Project;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 
 class MemberFactory extends Factory
 {
@@ -13,10 +14,19 @@ class MemberFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
-            'password' => Hash::make('password'),  // Default password
-            'phone_number' => $this->faker->phoneNumber,  // Fake phone number
+            'owner_id' => User::factory(),  // Creates a new User as the owner
+            'member_id' => User::factory(), // Creates a new User as the member
+            'project_id' => Project::factory(), // Creates a new Project associated with the member
+            'devices' => [
+                // Sample device permissions format
+                "1" => [
+                    "2" => "view",
+                    "3" => "control",
+                ],
+                "4" => [
+                    "5" => "view",
+                ],
+            ],  // JSON structure for devices and permissions
         ];
     }
 }
