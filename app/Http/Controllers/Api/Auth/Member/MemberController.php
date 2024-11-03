a4188c982dc3300e82846a7b6c1aa395cf510abf
bola-cloud <bola.ishak41@gmail.com>
2024-11-03 17:08:20 +0200

    commit

--- app/Http/Controllers/Api/Auth/Member/MemberController.php
+++ app/Http/Controllers/Api/Auth/Member/MemberController.php
@@ -162,7 +162,7 @@ class MemberController extends Controller
             "app_id" => env('ONESIGNAL_APP_ID'),
             "headings" => ["en" => "Access Granted to Project Devices"],
             "contents" => [
-                "en" => "You have been granted access to devices: " . implode(', ', $deviceNames)
+                "en" => "You have been granted access to devices: " 
             ],
             "data" => [
                 "type" => "access_granted",
