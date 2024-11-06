8febf0bc2058c8e2887da0bab7d02d285f770d2b
bola-cloud <bola.ishak41@gmail.com>
2024-11-06 14:11:35 +0200

    commit

--- app/Jobs/ExecuteConditionAction.php
+++ app/Jobs/ExecuteConditionAction.php
@@ -2,14 +2,14 @@
 
 namespace App\Jobs;
 
-use App\Models\Condition;
-use App\Models\Component;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Carbon\Carbon;
+use App\Models\Condition;
+use App\Models\Component;
 
 class ExecuteDeviceAction implements ShouldQueue
 {
