ad6b66b270279ff4f359e1473bd7c13ebd627ff8
bola-cloud <bola.ishak41@gmail.com>
2024-11-13 10:32:34 +0200

    df

--- app/Jobs/ExecuteConditionAction.php
+++ app/Jobs/ExecuteConditionAction.php
@@ -53,11 +53,12 @@ class ExecuteConditionAction implements ShouldQueue
         }
     
         Log::info("Condition found and case is active for condition {$this->conditionId}");
-    
+        Log::info("Bola");
+
         // Safely access the 'if' conditions with a fallback
         $ifLogic = $condition->cases['if']['logic'] ?? 'OR';
         $ifConditions = $condition->cases['if']['conditions'] ?? [];
-        Log::info("logic condition {$ifConditions} , {$ifLogic}");
+        Log::info("Condition found and case is active for condition {$ifConditions} ,{$ifLogic}");
         // Evaluate the "if" conditions
         Log::info("Evaluating 'if' conditions for condition {$this->conditionId}");
         if ($this->evaluateIfConditions($ifConditions, $ifLogic)) {
