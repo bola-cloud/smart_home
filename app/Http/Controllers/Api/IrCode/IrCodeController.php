6a5ee50db2bd547c35d2e6539a9b30d558c4bc64
bola-cloud <bola.ishak41@gmail.com>
2024-11-25 11:12:18 +0200

    dvdfs

--- app/Http/Controllers/Api/IrCode/IrCodeController.php
+++ app/Http/Controllers/Api/IrCode/IrCodeController.php
@@ -86,7 +86,7 @@ class IrCodeController extends Controller
         return response()->json([
             'file_name' => $fileName,
             'is_user_added' => $isUserAdded,
-            'content' => json_decode($fileContent),
+            'content' => json_encode($fileContent),
         ]);
     }
     
