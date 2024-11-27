37df11031a10c2556e755feb2aabb06764ac45e5
bola-cloud <bola.ishak41@gmail.com>
2024-11-27 15:59:04 +0200

    register

--- app/Http/Controllers/Api/MqttController.php
+++ app/Http/Controllers/Api/MqttController.php
@@ -26,46 +26,28 @@ class MqttController extends Controller
         $deviceId = $request->device_id;
         $componentId = $request->component_id;
         $message = $request->message;
+        $component = Component::find($componentId);
 
-        $result = $this->mqttService->publishAction($deviceId, $componentId, $message, true);
+        $result = $this->mqttService->publishAction($deviceId, $component->order, $message, true);
 
         return response()->json($result);
     }
 
     public function subscribeToTopic(Request $request)
     {
-        // dd($request->all());
         // Validate incoming data
         $request->validate([
             'device_id' => 'required|integer',
             'component_id' => 'required|integer',
         ]);
-
         // Get the validated data from the request
         $deviceId = $request->device_id;
         $componentId = $request->component_id;
+
+        $component = Component::find($componentId);
 
         // Redirect the request to the service to subscribe to the topic
-        $result = $this->mqttService->subscribeToTopic($deviceId, $componentId);
-
-        // Return the response from the service
-        return response()->json($result);
-    }
-
-    public function getLastMessage(Request $request)
-    {
-        // Validate incoming data
-        $request->validate([
-            'device_id' => 'required|integer',
-            'component_id' => 'required|integer',
-        ]);
-
-        // Get the validated data from the request
-        $deviceId = $request->device_id;
-        $componentId = $request->component_id;
-
-        // Redirect the request to the service to fetch the last message
-        $result = $this->mqttService->getLastMessage($deviceId, $componentId);
+        $result = $this->mqttService->subscribeToTopic($deviceId, $component->order);
 
         // Return the response from the service
         return response()->json($result);
