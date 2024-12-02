8865938fe357b766c4b24f3e7b6518a3ad2cc01f
bola-cloud <bola.ishak41@gmail.com>
2024-12-02 15:03:13 +0200

    sdcsd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Session;
+use Darryldecode\Cart\Facades\CartFacade as Cart;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Cart::session(Session::getId());
     }
 }
--- config/app.php
+++ config/app.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Session;
 
 return [
 
