d9b909dc4d46f964e521028b3c11a1dd7fea3e9e
bola-cloud <bola.ishak41@gmail.com>
2024-12-03 11:10:48 +0200

    checkout

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\Api\NotificationController;
 use App\Http\Controllers\Api\Conditions\ConditionsController;
 use App\Http\Controllers\Api\IrCode\IrCodeController;
 use App\Http\Controllers\Api\Products\ProductController;
-// use App\Http\Controllers\Api\Checkout\CheckoutController;
+use App\Http\Controllers\Api\Checkout\CheckoutController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
