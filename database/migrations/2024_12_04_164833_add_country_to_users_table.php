b01d7b5db4732eb6de595f0fb66d7a4061d859ce
bola-cloud <bola.ishak41@gmail.com>
2024-12-04 17:10:13 +0200

    regions

--- database/migrations/2024_12_04_164833_add_country_to_users_table.php
+++ database/migrations/2024_12_04_164833_add_country_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('country',['Egypt','Saudi']);
+            $table->enum('country',['Egypt','Saudi'])->default('Egypt');
         });
     }
 
