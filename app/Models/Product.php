a2057a6bdc5953759f926fe19821bc863a676215
bola-cloud <bola.ishak41@gmail.com>
2024-12-04 16:04:05 +0200

    regions

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -24,5 +24,9 @@ class Product extends Model
     {
         return $this->hasMany(CheckoutItem::class);
     }
-    
+
+    public function prices()
+    {
+        return $this->hasMany(CountryPrice::class);
+    }    
 }
---
+++ database/migrations/2024_12_04_155253_create_country_prices_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('country_prices', function (Blueprint $table) {
+            $table->id();
+            $table->enum('country',['Egypt','Saudi']);
+            $table->integer('price');
+
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('country_prices');
+    }
+};
