fb087ef998391775504f301bac9c690a3f723a75
bola-cloud <bola.ishak41@gmail.com>
2024-12-16 17:14:45 +0200

    device

--- resources/views/admin/devices/create.blade.php
+++ resources/views/admin/devices/create.blade.php
@@ -59,7 +59,7 @@
 
 @push('css')
 <!-- Select2 CSS -->
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
 <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 @endpush
