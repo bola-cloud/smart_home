85c744a1af2925c012b0ab369fffe4af6e80b779
bola-cloud <bola.ishak41@gmail.com>
2024-12-22 17:03:01 +0200

    User

--- resources/views/admin/users/details.blade.php
+++ resources/views/admin/users/details.blade.php
@@ -112,8 +112,12 @@
 @endsection
 
 @push('js')
-    <!-- Updated CDNs -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" integrity="sha512-dky/jy1vlzpXOuZVo+FcJm5VjJbBLkqirFc8W9MsUOFgM9bThR/AdfDiNKNqcT6H4gRfyxOXVMBPL/IhO8mijQ==" crossorigin="anonymous">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQNNtgsOI4h6uwJ3w84dNH6sZMAePQ3Z5mC1o8F3L9fUklm5G95LGFBrC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-5CmzC02R9TyfVgAWiP2U66WLCg6l3TmaT9EfU+1hSljf7LOVW3tvk9DAmH+9354d" crossorigin="anonymous"></script>
+   <!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQNNtgsOI4h6uwJ3w84dNH6sZMAePQ3Z5mC1o8F3L9fUklm5G95LGFBrC" crossorigin="anonymous">
+
+<!-- Bootstrap JS Bundle -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+<!-- FontAwesome -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endpush
