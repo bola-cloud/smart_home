a8a124a5add5a2da0e01d0d36aed6a508c21090f
bola-cloud <bola.ishak41@gmail.com>
2025-01-08 16:15:26 +0200

    seeder

--- app/Http/Controllers/Admin/Pricing/RoomDeviceController.php
+++ app/Http/Controllers/Admin/Pricing/RoomDeviceController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin\Pricing;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Room;
+use App\Models\RoomDevice;
 
 class RoomDeviceController extends Controller
 {
