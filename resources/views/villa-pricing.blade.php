991311f21368396ad8a628012309a6260f5c45dc
bola-cloud <bola.ishak41@gmail.com>
2025-01-08 18:00:11 +0200

    seeder

--- resources/views/villa-pricing.blade.php
+++ resources/views/villa-pricing.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="apple-touch-icon" href="{{asset("app-assets/images/logo/mazaya-logo-dark.png")}}">
-    <title>تقدير تكلفة الفلة</title>
+    <title>تقدير تكلفة </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <style>
@@ -22,7 +22,7 @@
     <div class="container my-5">
         <div class="text-center">
             <img src="{{asset('app-assets/images/logo/mazaya-logo-dark.png')}}" alt="Logo" class="logo">
-            <h1>تقدير تكلفة الفلة</h1>
+            <h1>تقدير تكلفة الي المنزل او الفيلا</h1>
         </div>
 
         @if ($errors->any())
