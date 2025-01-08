8d0905addd83d994c129d546d2fc6851860d6c33
bola-cloud <bola.ishak41@gmail.com>
2025-01-08 18:21:30 +0200

    seeder

--- resources/views/villa-result.blade.php
+++ resources/views/villa-result.blade.php
@@ -100,9 +100,9 @@
             </div>
         </div>
         <div class="card mb-4">
-            <div class="card-header bg-secondary text-dark">إجمالي التكلفة</div>
+            <div class="card-header bg-secondary text-dark">ملحوظة:</div>
             <div class="card-body">
-                ملحوظة : هذه تكلفه الاجهزه فقط لا تشمل تكلفه اي اسلاك او امدادت كهربيه او امدادت خاصه بالانترنت 
+                 : هذه تكلفه الاجهزه فقط لا تشمل تكلفه اي اسلاك او امدادت كهربيه او امدادت خاصه بالانترنت 
             </div>
         </div>
     </div>
