9a78d2aa83d4a47c10b52bbfa9264cef893a12f5
bola-cloud <bola.ishak41@gmail.com>
2025-01-08 17:52:28 +0200

    seeder

--- resources/views/villa-result.blade.php
+++ resources/views/villa-result.blade.php
@@ -29,7 +29,7 @@
 <body>
     <div class="container my-5">
         <div class="text-center">
-            <img src="{{asset('app-assets/images/logo/mazaya-logo-dark.png')}}" alt="Logo" class="logo"> <strong>Mazaya</strong> 
+            <img src="{{asset('app-assets/images/logo/mazaya-logo-dark.png')}}" alt="Logo" class="logo"> <strong style="font-size: x-large;">Mazaya</strong> 
             <h1>نتيجة تقدير التكلفة</h1>
         </div>
 
